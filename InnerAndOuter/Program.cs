bea25c47236975fdec41be7e206209b6dc1d7ab5
Opilane TTHK <opilane@tthk.ee>
2020-12-04 09:43:30 +0200

    first commit

---
+++ InnerAndOuter/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace InnerAndOuter
+{
+
+    class Program
+    {
+        public class Item
+        {
+            string planet;
+            int mass;
+
+            public Item(string _name, int _price)
+            {
+                planet = _name;
+                mass = _price;
+
+            }
+
+            public string Name { get { return planet; } }
+            public int Price { get { return mass; } }
+        }
+        static void Main(string[] args)
+        {
+            InnerPlanetList();
+            OuterPlanetList();
+        }
+
+        public static void InnerPlanetList()
+        {
+            List<string> planetList = new List<string>();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("enter planet: ");
+                string item = Console.ReadLine();
+                Console.WriteLine("enter mass: ");
+                string price = Console.ReadLine();
+
+                string line = $"{item}${price}";
+                planetList.Add(line);
+            }
+            string filePath = @"C:\Users\opilane\samples";
+            string fileName = @"innerPlanets.txt";
+            File.WriteAllLines(Path.Combine(filePath, fileName), planetList);
+
+            
+
+
+        }
+
+        public static void OuterPlanetList()
+        {
+            List<string> planetList = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("enter planet: ");
+                string item = Console.ReadLine();
+                Console.WriteLine("enter mass: ");
+                string price = Console.ReadLine();
+
+                string line = $"{item}${price}";
+                planetList.Add(line);
+            }
+            string filePath = @"C:\Users\opilane\samples";
+            string fileName = @"OuterPlanets.txt";
+            File.WriteAllLines(Path.Combine(filePath, fileName), planetList);
+        }
+
+
+    }
+
+}
---
+++ OOP Solar System/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OOPSolarSystem
+{
+    class Program
+    {
+
+        
+        public class Planet
+        {
+            string name;
+            int price;
+
+            public Planet(string _name, int _price)
+            {
+                name = _name;
+                price = _price;
+
+            }
+            public string Name { get { return name; } }
+            public int Price { get { return price; } }
+            static void Main(string[] args)
+            {
+                ReadFromPlanetList();
+            }
+            public static void ReadFromPlanetList()
+            {
+                string filePath = @"C:\Users\opilane\samples";
+                string fileName = @"planets.txt";
+
+                List<Planet> planetItems = new List<Planet>();
+
+                List<string> linesFromFile = File.ReadAllLines(Path.Combine(filePath, fileName)).ToList();
+
+                foreach (string line in linesFromFile)
+                {
+                    string[] teampArray = line.Split(new char[] { '$' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    Planet newItem = new Planet(teampArray[0], Int32.Parse(teampArray[1]));
+                    planetItems.Add(newItem);
+
+
+
+                }
+
+                Console.WriteLine("Your planets:");
+                int total = 0;
+                foreach (Planet item in planetItems)
+                {
+                    Console.WriteLine($"Planet: {item.Name}); Price: {item.Price }");
+                    total += item.Price;
+                }
+
+
+            }
+        }
+    }
+}
